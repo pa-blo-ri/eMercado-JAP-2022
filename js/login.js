707ce981e90fcf741fcdce2dca72d1b09ff62fe0
pa-blo-ri <110500127+pa-blo-ri@users.noreply.github.com>
2022-11-10 12:46:28 -0300

    Update FB

--- js/login.js
+++ js/login.js
@@ -39,4 +39,25 @@ function isEmptyStyle(tagId) {
 
 }
 
-document.getElementById('forg-pass').onclick = () => signInWithPopup()
+function callLoginGoogle(){
+    signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+}
+
+document.getElementById('forg-pass').onclick = () => callLoginGoogle()
