b7ee3ff7386dbd4396f1b17e520e0ab25d423b41
pa-blo-ri <110500127+pa-blo-ri@users.noreply.github.com>
2022-11-16 08:12:04 -0300

    fix firebase

--- js/initialize-firebase.js
+++ js/initialize-firebase.js
@@ -1,6 +1,6 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.13.0/firebase/app';
-    import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.13.0/firebase/firestore/lite';
-    import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.13.0/firebase/auth";
+import { initializeApp } from 'firebase/app';
+    import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+    import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
     // Follow this pattern to import other Firebase services
     // import { } from 'firebase/<service>';
 
