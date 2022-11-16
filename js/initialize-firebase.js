b393f1818ba98ee655f3fb5c6401f4a5f3b40184
pa-blo-ri <110500127+pa-blo-ri@users.noreply.github.com>
2022-11-16 09:48:47 -0300

    lwg

--- js/initialize-firebase.js
+++ js/initialize-firebase.js
@@ -1,66 +1,67 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js';
-    import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js';
-    import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
-    // Follow this pattern to import other Firebase services
-    // import { } from 'firebase/<service>';
+import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
 
-    // TODO: Replace the following with your app's Firebase project configuration
-    const firebaseConfig = {
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
 
-      apiKey: "AIzaSyAE1spC1Frze7kr4aP-NrcjZHGxSnAbz_Q",
+    apiKey: "AIzaSyAE1spC1Frze7kr4aP-NrcjZHGxSnAbz_Q",
 
-      authDomain: "proyecto-jap-c66f2.firebaseapp.com",
+    authDomain: "proyecto-jap-c66f2.firebaseapp.com",
 
-      projectId: "proyecto-jap-c66f2",
+    projectId: "proyecto-jap-c66f2",
 
-      storageBucket: "proyecto-jap-c66f2.appspot.com",
+    storageBucket: "proyecto-jap-c66f2.appspot.com",
 
-      messagingSenderId: "228063077799",
+    messagingSenderId: "228063077799",
 
-      appId: "1:228063077799:web:95475790fe3620af2c0282"
+    appId: "1:228063077799:web:95475790fe3620af2c0282"
 
-    };
+};
 
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-    // Get a list of cities from your database
-    async function getCities(db) {
-      const citiesCol = collection(db, 'cities');
-      const citySnapshot = await getDocs(citiesCol);
-      const cityList = citySnapshot.docs.map(doc => doc.data());
-      return cityList;
+// Get a list of cities from your database
+async function getCities(db) {
+    const citiesCol = collection(db, 'cities');
+    const citySnapshot = await getDocs(citiesCol);
+    const cityList = citySnapshot.docs.map(doc => doc.data());
+    return cityList;
+}
+
+
+const loginWithGoogle = () => {
+
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    
+    signInWithPopup(auth, provider)
+        .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+        console.log(user.displayName)
+        }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+    
+        console.log(errorMessage)
+        
+        });
+    
     }
-
-    /*
-   const loginWithGoogle = () => {
-
-        const provider = new GoogleAuthProvider();
-        const auth = getAuth();
-      
-        signInWithPopup(auth, provider)
-          .then((result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            // ...
-            console.log(user.displayName)
-          }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
-      
-            console.log(errorMessage)
-            
-          });
-      
-      }
-      */
+    
+export { loginWithGoogle }
--- js/login.js
+++ js/login.js
@@ -10,34 +10,3 @@ document.getElementById('formulario').onsubmit = (e) => {
     }
     document.getElementById('formulario').classList.add('was-validated')
 }
-
-const loginWithGoogle = () => {
-
-    const provider = new GoogleAuthProvider();
-    const auth = getAuth();
-  
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        // The signed-in user info.
-        const user = result.user;
-        // ...
-        console.log(user.displayName)
-      }).catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
-  
-        console.log(errorMessage)
-        
-      });
-  
-  }
-
